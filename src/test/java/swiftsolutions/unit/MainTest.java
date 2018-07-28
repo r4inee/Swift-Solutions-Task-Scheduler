0a7a9d23fd9347f310c93b39f644af07f6ff79f7
Simon Su <f-7-beihan@hotmail.com>
2018-07-28 21:53:09 +1200

    testing dot file parser tool

--- src/test/java/swiftsolutions/unit/MainTest.java
+++ src/test/java/swiftsolutions/unit/MainTest.java
@@ -1,13 +1,33 @@
 package swiftsolutions.unit;
 
+import com.paypal.digraph.parser.GraphEdge;
+import com.paypal.digraph.parser.GraphNode;
+import com.paypal.digraph.parser.GraphParser;
 import org.junit.Test;
 
-import static junit.framework.Assert.assertTrue;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Map;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.fail;
+
 
 public class MainTest {
 
     @Test
     public void testMain() {
-        assertTrue(true);
+        try {
+            GraphParser parser = new GraphParser(new FileInputStream("src/test/resources/test.dg"));
+            Map<String, GraphNode> nodes = parser.getNodes();
+
+            Map<String, GraphEdge> edges = parser.getEdges();
+
+            assertEquals(4, nodes.values().size());
+            assertEquals(4, edges.values().size());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            fail();
+        }
     }
 }
