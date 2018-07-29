aadcfc2706d1f0b2d60a4331f10d50869ad9942a
Kerwin Sun <ksun182@UoA.auckland.ac.nz>
2018-07-29 13:21:01 +1200

    changed times to type long

--- src/main/java/swiftsolutions/TaskScheduler/Processor.java
+++ src/main/java/swiftsolutions/TaskScheduler/Processor.java
@@ -1,31 +1,36 @@
 package swiftsolutions.TaskScheduler;
 
+<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+=======
+import java.util.List;
+>>>>>>> 366f1ab... project skeleton and architecture from meeting 1 (28/07/18)
 
 /**
  * This class represent a processor object which contains the current schedule allocated by the scheduler.
  */
 public class Processor {
     // Represent the current list of tasks it is performing.
-    private Map<Task, int[]> _taskList;
+<<<<<<< HEAD
+    private Map<Task, Long[]> _taskList;
     // Represent the current finish time of this processor
-    private int _endTime;
+    private Long _endTime;
     
     public Processor(){
     	
-    	_taskList = new HashMap<Task, int[]>();
+    	_taskList = new HashMap<Task, Long[]>();
     }
     
-    public int getEndTime(){
+    public Long getEndTime(){
 		
     	
     	return _endTime;
     }
     
-    public Map<Task, int[]> getTaskList(){
+    public Map<Task, Long[]> getTaskList(){
 		
     	
     	return _taskList;
@@ -33,9 +38,14 @@ public class Processor {
     
     public void addTask(Task task, int offset){
     	
-    	int[] startAndLengthTimes = new int[2];
+    	Long[] startAndLengthTimes = new Long[2];
     	startAndLengthTimes[0] = _endTime + offset;
-    	startAndLengthTimes[1] = startAndLengthTimes + task.processTime; // TODO: 
+    	startAndLengthTimes[1] = startAndLengthTimes[1] + task.getProcessTime(); // TODO: 
     	_taskList.put(task, startAndLengthTimes);
     }
+=======
+    private List<Task> taskList;
+    // Represent the current finish time of this processor
+    private int endTime;
+>>>>>>> 366f1ab... project skeleton and architecture from meeting 1 (28/07/18)
 }
--- src/main/java/swiftsolutions/TaskScheduler/Schedule.java
+++ src/main/java/swiftsolutions/TaskScheduler/Schedule.java
@@ -5,7 +5,10 @@ package swiftsolutions.TaskScheduler;
  */
 public abstract class Schedule {
 
+<<<<<<< HEAD
 	
 
 
+=======
+>>>>>>> 366f1ab... project skeleton and architecture from meeting 1 (28/07/18)
 }
