b9b9be40d6de17699cee03b1c29d0d8d81731d3f
Bowen Zheng <bowenzheng998@gmail.com>
2018-07-29 12:36:35 +1200

    Added constructor, and fields that contain the children tasks and processing time

--- src/main/java/swiftsolutions/TaskScheduler/Task.java
+++ src/main/java/swiftsolutions/TaskScheduler/Task.java
@@ -1,8 +1,23 @@
 package swiftsolutions.TaskScheduler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This class represent a single task which needs to be scheduled and is parsed from nodes of an input graph.
  */
 public class Task {
 
+    private long _processTime;
+    private int _ingoingEdges;
+    private List<Task> _childList = new ArrayList<>();
+
+
+    public Task(long processTime){
+        this._processTime = processTime;
+    }
+
+    public List<Task> getChildList(){
+        return this._childList;
+    }
 }
