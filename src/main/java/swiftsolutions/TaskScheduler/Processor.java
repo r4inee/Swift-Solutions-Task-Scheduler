ecac5d513af06bf4782bef59a602250c4c7482fd
Kerwin Sun <ksun182@UoA.auckland.ac.nz>
2018-07-29 13:21:01 +1200

    changed times to type long

--- src/main/java/swiftsolutions/TaskScheduler/Processor.java
+++ src/main/java/swiftsolutions/TaskScheduler/Processor.java
@@ -10,22 +10,22 @@ import java.util.Map;
  */
 public class Processor {
     // Represent the current list of tasks it is performing.
-    private Map<Task, int[]> _taskList;
+    private Map<Task, Long[]> _taskList;
     // Represent the current finish time of this processor
-    private int _endTime;
+    private Long _endTime;
     
     public Processor(){
     	
-    	_taskList = new HashMap<Task, int[]>();
+    	_taskList = new HashMap<Task, Long[]>();
     }
     
-    public int getEndTime(){
+    public Long getEndTime(){
 		
     	
     	return _endTime;
     }
     
-    public Map<Task, int[]> getTaskList(){
+    public Map<Task, Long[]> getTaskList(){
 		
     	
     	return _taskList;
@@ -33,9 +33,9 @@ public class Processor {
     
     public void addTask(Task task, int offset){
     	
-    	int[] startAndLengthTimes = new int[2];
+    	Long[] startAndLengthTimes = new Long[2];
     	startAndLengthTimes[0] = _endTime + offset;
-    	startAndLengthTimes[1] = startAndLengthTimes + task.processTime; // TODO: 
+    	startAndLengthTimes[1] = startAndLengthTimes[1] + task.getProcessTime(); // TODO: 
     	_taskList.put(task, startAndLengthTimes);
     }
 }
