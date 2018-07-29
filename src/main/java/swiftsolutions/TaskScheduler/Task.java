f019ecd7ba3ffb07a407c8835b72a57c6937ab63
Bowen Zheng <bowenzheng998@gmail.com>
2018-07-29 12:56:00 +1200

    Reimplemented a list of all the children, added methods to add child tasks, increment ingoing edge count

--- src/main/java/swiftsolutions/TaskScheduler/Task.java
+++ src/main/java/swiftsolutions/TaskScheduler/Task.java
@@ -11,15 +11,54 @@ import java.util.Map;
 public class Task {
 
     private long _processTime;
-    private int _ingoingEdges;
-    private Map<Task,Integer> childTaskMap = new HashMap<>(); //Used to map a child and the edge weight to the child
+    private int _ingoingEdges; //Default 0
+    private Map<Task,Integer> _childTaskMap = new HashMap<>(); //Used to map a child and the edge weight to the child
+    private List<Task> _childList = new ArrayList<>(); //Store the children
 
     /*
     Constructor for Task that takes in the process time for the task
      */
     public Task(long processTime){
-        this._processTime = processTime;
+        _processTime = processTime;
     }
 
-    
+    /**
+     * Method to add a child task to the task
+     * @param childTask
+     * @param edgeWeight
+     */
+    public void addChild(Task childTask, Integer edgeWeight){
+        childTask.increaseIngoingEdges();
+        _childTaskMap.put(childTask,edgeWeight);
+        _childList.add(childTask);
+    }
+
+    /**
+     * Method to get the communication time to the task in the input
+     * parameter where the task in the input parameter is a child
+     * of this task
+     * @param nextTask
+     * @return
+     */
+    public Integer getCommunicationTime(Task nextTask){
+        return _childTaskMap.get(nextTask);
+    }
+
+    /**
+     * Method that returns the list of all child tasks
+     * from this task
+     * @return
+     */
+    public List<Task> getChildList(){
+        return _childList;
+    }
+
+    /**
+     * Increases the count of ingoing edges to the task.
+     * This is only incremented when the task is added to
+     * a parent task
+     */
+    private void increaseIngoingEdges(){
+        _ingoingEdges++;
+    }
 }
