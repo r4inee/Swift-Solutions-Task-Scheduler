f34771306c4107422b84f4523ae9f717e5634c07
Bowen Zheng <bowenzheng998@gmail.com>
2018-07-29 12:45:59 +1200

    Changed the implementation for containing children to a Map to include edge weight

--- src/main/java/swiftsolutions/TaskScheduler/Task.java
+++ src/main/java/swiftsolutions/TaskScheduler/Task.java
@@ -1,7 +1,9 @@
 package swiftsolutions.TaskScheduler;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * This class represent a single task which needs to be scheduled and is parsed from nodes of an input graph.
@@ -10,14 +12,14 @@ public class Task {
 
     private long _processTime;
     private int _ingoingEdges;
-    private List<Task> _childList = new ArrayList<>();
+    private Map<Task,Integer> childTaskMap = new HashMap<>(); //Used to map a child and the edge weight to the child
 
-
+    /*
+    Constructor for Task that takes in the process time for the task
+     */
     public Task(long processTime){
         this._processTime = processTime;
     }
 
-    public List<Task> getChildList(){
-        return this._childList;
-    }
+    
 }
