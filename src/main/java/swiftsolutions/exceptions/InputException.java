0991d8157635195bc3adc2cafcfda70fc8feaabc
Simon Su <f-7-beihan@hotmail.com>
2018-07-31 16:55:13 +1200

    added input exceptions class

--- src/main/java/swiftsolutions/exceptions/InputException.java
+++ src/main/java/swiftsolutions/exceptions/InputException.java
@@ -1,5 +1,9 @@
 package swiftsolutions.exceptions;
 
+/**
+ * This class is an checked exception handling for error for user input.
+ * This exception is thrown when the input file cannot be found or if the file cannot be parsed correctly.
+ */
 public class InputException extends Exception {
     public InputException() {
         super();
