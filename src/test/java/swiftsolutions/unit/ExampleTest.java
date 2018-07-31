8657a0bd91d2cf360960e27bc76ea66ef3ef346e
Simon Su <f-7-beihan@hotmail.com>
2018-07-31 21:49:57 +1200

    Unit test for DOTInputParser class

---
+++ src/test/java/swiftsolutions/unit/ExampleTest.java
@@ -0,0 +1,15 @@
+package swiftsolutions.unit;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * This is an example of naming convention for unit test, Name with suffix Test.
+ */
+public class ExampleTest {
+    @Test
+    public void testExample() {
+        assertTrue(true);
+    }
+}
