e2f353903fc7aed6cd6ac1f05a1712d18725b8c4
kerwin10010 <kerwin10010@gmail.com>
2018-08-01 15:13:28 +1200

    changed processor class to use tuples

--- src/main/java/swiftsolutions/taskscheduler/Processor.java
+++ src/main/java/swiftsolutions/taskscheduler/Processor.java
@@ -3,33 +3,34 @@ package swiftsolutions.taskscheduler;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.antlr.v4.runtime.misc.Pair;
+
 /**
  * This class represent a processor object which contains the current schedule allocated by the scheduler.
  */
 public class Processor {
     // Represent the current list of tasks it is performing.
-    private Map<swiftsolutions.taskscheduler.Task, Long[]> _taskList;
+    private Map<Task, Pair<Long, Long>> _taskList;
     // Represent the current finish time of this processor
     private Long _endTime;
     
     public Processor(){
-    	_taskList = new HashMap<Task, Long[]>();
+    	_taskList = new HashMap<Task, Pair<Long, Long>>();
     }
     
     public Long getEndTime(){
     	return _endTime;
     }
     
-    public Map<Task, Long[]> getTaskList(){
+    public Map<Task, Pair<Long, Long>> getTaskList(){
 		
     	
     	return _taskList;
     }
     
     public void addTask(Task task, int offset){
-    	Long[] startAndLengthTimes = new Long[2];
-    	startAndLengthTimes[0] = _endTime + offset;
-    	startAndLengthTimes[1] = startAndLengthTimes[1] + task.getProcessTime(); // TODO:
-    	_taskList.put(task, startAndLengthTimes);
+    	Long taskStart = _endTime + offset;
+    	Long taskEnd = taskStart + task.getProcessTime(); 
+    	_taskList.put(task, new Pair<Long, Long>(taskStart, taskEnd));
     }
 }
