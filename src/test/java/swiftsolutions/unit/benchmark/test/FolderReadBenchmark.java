316b66dc7dc5f25d3a948de7ef77554090791b72
kerwin10010 <kerwin10010@gmail.com>
2018-08-02 13:03:22 +1200

    added tests for benchmark catagoriser

---
+++ src/test/java/swiftsolutions/unit/benchmark/test/FolderReadBenchmark.java
@@ -0,0 +1,36 @@
+package swiftsolutions.unit.benchmark.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import swiftsolutions.unit.benchmark.BenchmarkParser;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ *Test the ability of the benchmarkParser to generate and catagorise files
+ */
+public class FolderReadBenchmark {
+    
+	BenchmarkParser _benchmarkParser;
+	
+	@Before
+	public void generateBenchmark() {
+		
+		_benchmarkParser = new BenchmarkParser("src/test/resources/test_graphs_full_name");
+		_benchmarkParser.catagoriseFiles();
+
+		
+	}
+	
+	@Test
+    public void testExample() {
+        
+		assertEquals( 3 , _benchmarkParser.getNodesCatagory().keySet().size());
+		assertEquals( 4 , _benchmarkParser.getProcessorCatagory().keySet().size());
+		assertEquals( 14 , _benchmarkParser.getTypeCatagory().keySet().size());
+
+    }
+}
