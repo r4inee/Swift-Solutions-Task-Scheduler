0d96632b41f2dcf7dc7553e8d7acfb0c6635a7c6
Bowen Zheng <bowenzheng998@gmail.com>
2018-08-04 19:12:40 +1200

    Removed the second DOTInputParser test suite, added comments to one test case in CLIOptionTest implemented test suite for DOTInputParser

--- src/test/java/swiftsolutions/unit/DOTInputParserTest.java
+++ src/test/java/swiftsolutions/unit/DOTInputParserTest.java
@@ -1,5 +1,7 @@
 package swiftsolutions.unit;
 
+import javafx.scene.Parent;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import swiftsolutions.exceptions.InputException;
@@ -20,47 +22,159 @@ import static junit.framework.Assert.fail;
 
 public class DOTInputParserTest {
 
+    DOTInputParser _parser;
+    Map<Integer, Task> _taskMap;
+
+    /**
+     * This method initializes a new DOTInputParser
+     * and sets the map to be null before every test
+     * is executed
+     */
+    @Before
+    public void init(){
+        _parser = new DOTInputParser();
+        _taskMap = null;
+    }
+
+    /**
+     * This test case test that for when no headers
+     * are passed in to parse(), the graph is correctly
+     * parsed. Where the nodes have the correct number of dependencies
+     */
     @Test
     public void testParseNoHeaders() {
-        DOTInputParser parser = new DOTInputParser();
-        Map<Integer, Task> taskMap = null;
         try {
-            taskMap = parser.parse("src/test/resources/test_graphs/Nodes_7_OutTree.dot");
+            _taskMap = _parser.parse("src/test/resources/test_graphs/Nodes_7_OutTree.dot");
         } catch (InputException e) {
             e.printStackTrace();
             fail();
         }
 
-        assertNotNull(taskMap);
-        assertEquals(7, taskMap.size());
+        assertNotNull(_taskMap);
+        assertEquals(7, _taskMap.size());
 
-        for (Integer task : taskMap.keySet()) {
+        for (Integer task : _taskMap.keySet()) {
             switch (task) {
                 case 0:
-                    assertEquals(0, taskMap.get(task).getNumDependency());
+                    assertEquals(0, _taskMap.get(task).getNumDependency());
                     break;
                 case 1:
-                    assertEquals(1, taskMap.get(task).getNumDependency());
+                    assertEquals(1, _taskMap.get(task).getNumDependency());
                     break;
                 case 2:
-                    assertEquals(1, taskMap.get(task).getNumDependency());
+                    assertEquals(1, _taskMap.get(task).getNumDependency());
                     break;
                 case 3:
-                    assertEquals(1, taskMap.get(task).getNumDependency());
+                    assertEquals(1, _taskMap.get(task).getNumDependency());
                     break;
                 case 4:
-                    assertEquals(1, taskMap.get(task).getNumDependency());
+                    assertEquals(1, _taskMap.get(task).getNumDependency());
                     break;
                 case 5:
-                    assertEquals(1, taskMap.get(task).getNumDependency());
+                    assertEquals(1, _taskMap.get(task).getNumDependency());
                     break;
                 case 6:
-                    assertEquals(1, taskMap.get(task).getNumDependency());
+                    assertEquals(1, _taskMap.get(task).getNumDependency());
                     break;
                 default:
                     fail("Incorrectly parsed edges of the graph");
             }
         }
     }
+
+    /**
+     * Test for an invalid file path passed into parse()
+     * the correct exception is thrown with the correct message
+     */
+    @Test
+    public void testParseInvalidFile(){
+        try {
+            _taskMap = _parser.parse("foo");
+        } catch (InputException e) {
+            assertEquals("Input graph file not found",e.getMessage());
+        }
+    }
+
+    /**
+     * Test for a valid file that the weights are correctly parsed
+     * for each node into the process time for the respective task
+     */
+    @Test
+    public void testParseProcessTimes(){
+        try {
+            _taskMap = _parser.parse("src/test/resources/test_graphs/Nodes_7_OutTree.dot");
+        } catch (InputException e) {
+            fail();
+        }
+        assertNotNull(_taskMap);
+        assertEquals(7,_taskMap.size());
+
+        for (Integer task : _taskMap.keySet()) {
+            switch (task) {
+                case 0:
+                    assertEquals(5, _taskMap.get(task).getProcessTime());
+                    break;
+                case 1:
+                    assertEquals(6, _taskMap.get(task).getProcessTime());
+                    break;
+                case 2:
+                    assertEquals(5, _taskMap.get(task).getProcessTime());
+                    break;
+                case 3:
+                    assertEquals(6, _taskMap.get(task).getProcessTime());
+                    break;
+                case 4:
+                    assertEquals(4, _taskMap.get(task).getProcessTime());
+                    break;
+                case 5:
+                    assertEquals(7, _taskMap.get(task).getProcessTime());
+                    break;
+                case 6:
+                    assertEquals(7, _taskMap.get(task).getProcessTime());
+                    break;
+                default:
+                    fail("Incorrectly parsed edges of the graph");
+            }
+        }
+    }
+
+    @Test
+    public void testParseCommunicationTimes(){
+        try {
+            _taskMap = _parser.parse("src/test/resources/test_graphs/Nodes_7_OutTree.dot");
+        } catch (InputException e) {
+            fail();
+        }
+        assertNotNull(_taskMap);
+        assertEquals(7,_taskMap.size());
+
+        for (Integer task : _taskMap.keySet()){
+            switch (task){
+                case 0:
+                    break;
+                case 1:
+                    assertEquals(15,_taskMap.get(task).getCommunicationCosts(0));
+                    break;
+                case 2:
+                    assertEquals(11,_taskMap.get(task).getCommunicationCosts(0));
+                    break;
+                case 3:
+                    assertEquals(11,_taskMap.get(task).getCommunicationCosts(0));
+                    break;
+                case 4:
+                    assertEquals(19,_taskMap.get(task).getCommunicationCosts(1));
+                    break;
+                case 5:
+                    assertEquals(4,_taskMap.get(task).getCommunicationCosts(1));
+                    break;
+                case 6:
+                    assertEquals(21,_taskMap.get(task).getCommunicationCosts(1));
+                    break;
+                default:
+                    fail("Incorrectly parsed edges of the graph");
+            }
+        }
+    }
+
 }
 
