25af18e42e294a28430431437256bc253d06fd98
kerwin10010 <kerwin10010@gmail.com>
2018-08-04 23:15:33 +1200

    Fully functioning benchmarking tool, runs all graphs in folder, can specify catagory and records all run times, execution times out after x seconds and moves onto next graph

--- src/test/java/swiftsolutions/unit/benchmark/test/GraphRunnerBenchmark.java
+++ src/test/java/swiftsolutions/unit/benchmark/test/GraphRunnerBenchmark.java
@@ -25,7 +25,7 @@ public class GraphRunnerBenchmark {
 	@Before
 	public void generateBenchmark() throws Exception {
 		
-		_benchmarkParser = new BenchmarkParser("src/test/resources/test_graphs");
+		_benchmarkParser = new BenchmarkParser("src/test/resources/test_graphs_full_name/");
 		_benchmarkParser.catagoriseFiles();
 		_runner = new BenchmarkAppRunner(1);
 
@@ -33,15 +33,15 @@ public class GraphRunnerBenchmark {
 	}
 	
 	@Test
-    public void testExample() {
+    public void runTest() {
         
 		ArrayList<File> nodeGraphs10 = _benchmarkParser.getNodesCatagory().get("10");
 		ArrayList<File> allGraphs = _benchmarkParser.getAllGraphs();
 		//_runner.addSingle(new File("C:/Users/User/Documents/uni/306/project1/SOFTENG306_Project1/src/test/resources/test_graphs_full_name/16p_Fork_Join_Nodes_10_CCR_0.10_WeightType_Random.dot"));
 		//_runner.addSingle(new File("C:/Users/User/Documents/uni/306/project1/SOFTENG306_Project1/src/test/resources/test_graphs_full_name/16p_Fork_Join_Nodes_21_CCR_0.10_WeightType_Random.dot"));
 		_runner.addList(allGraphs);
-		_runner.run();
+		_runner.runAll();
 		System.out.println(_runner.getOutputs().toString());
-
+		assertEquals(allGraphs.size() , _runner.getOutputs().size());
     }
 }
