f8c6bd51b105d70aaf2b29f60a598e0d488b829a
WanniCode <hwan678@aucklanduni.ac.nz>
2018-08-05 13:31:27 +1200

    added basic structure for testing BNBSchedule class

--- src/test/java/swiftsolutions/unit/SchedulingAlgorithmFactoryTest.java
+++ src/test/java/swiftsolutions/unit/SchedulingAlgorithmFactoryTest.java
@@ -1,10 +1,31 @@
 package swiftsolutions.unit;
 
+import org.junit.Before;
+import org.junit.Test;
+import swiftsolutions.interfaces.taskscheduler.Algorithm;
+import swiftsolutions.taskscheduler.Algorithms;
+import swiftsolutions.taskscheduler.SchedulingAlgorithmFactory;
+import swiftsolutions.taskscheduler.branchandbound.BNBAlgorithm;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by Harith on 03/08/2018.
  */
 public class SchedulingAlgorithmFactoryTest {
 
+        private SchedulingAlgorithmFactory _algoFac;
+
+        @Before
+        public void testStateInit() {
+                _algoFac = new SchedulingAlgorithmFactory();
+        }
+
+        @Test
+        public void testGetAlgorithm_BNB_nonParallel() {
+                assertTrue(_algoFac.getAlgorithm(Algorithms.BRANCH_AND_BOUND, 2, 0) instanceof BNBAlgorithm);
+
+        }
 
 
 
