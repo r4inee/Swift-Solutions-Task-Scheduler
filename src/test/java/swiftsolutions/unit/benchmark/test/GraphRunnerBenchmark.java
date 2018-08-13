cbeaf64663c329539fd88cf47a88a08a6625e951
kerwin10010 <kerwin10010@gmail.com>
2018-08-13 17:02:37 +1200

    added comments to all code

--- src/test/java/swiftsolutions/unit/benchmark/test/GraphRunnerBenchmark.java
+++ src/test/java/swiftsolutions/unit/benchmark/test/GraphRunnerBenchmark.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import org.junit.Before;
 import org.junit.Test;
 
+import swiftsolutions.taskscheduler.Schedule;
 import swiftsolutions.unit.benchmark.BenchmarkAppRunner;
 import swiftsolutions.unit.benchmark.BenchmarkParser;
 
@@ -21,19 +22,29 @@ public class GraphRunnerBenchmark {
     
 
 	/*
-	 * tests if it can run every small graph, without timeouts
+	 * Used to run all all lecturer provided graphs
 	 */
 	@Test
     public void runShortName() {
 		
 		BenchmarkAppRunner _runner = new BenchmarkAppRunner(1);
 		BenchmarkParser _benchmarkParser = new BenchmarkParser("src/test/resources/test_graphs/");
+
 		ArrayList<File> allGraphs = _benchmarkParser.getAllGraphs();
 		_runner.addList(allGraphs);
 		_runner.runAll();
 		assertEquals(allGraphs.size() , _runner.getOutputs().size());
 		assertEquals(0, _runner.getTimedOutGraphs().size());
+		
+		//test each graph gave correct outputs
+		System.out.println(_runner.getOutputs().keySet());
+		
+		
     }
+	
+	/*
+	 * Used to run all full name graphs
+	 */
 	
 	@Test
     public void runFullNameGraphs() {
@@ -46,7 +57,7 @@ public class GraphRunnerBenchmark {
 		//_runner.addSingle(new File("C:/Users/User/Documents/uni/306/project1/SOFTENG306_Project1/src/test/resources/test_graphs_full_name/16p_Fork_Join_Nodes_10_CCR_0.10_WeightType_Random.dot"));
 		//_runner.addSingle(new File("C:/Users/User/Documents/uni/306/project1/SOFTENG306_Project1/src/test/resources/test_graphs_full_name/16p_Fork_Join_Nodes_21_CCR_0.10_WeightType_Random.dot"));
 		_runner.addList(nodeGraphs10);
-		_runner.runAll();
-		System.out.println(_runner.getOutputs().toString());
+		//_runner.runAll();
+		//System.out.println(_runner.getOutputs().toString());
     }
 }
